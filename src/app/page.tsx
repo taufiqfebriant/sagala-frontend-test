cbdb54480933d85b5c9c2a84823ba3b754ca6e09
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-01 19:22:38 +0700

    Add development table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { DevelopmentTable } from "./development-table";
 import { FourColumnTable } from "./four-column-table";
 
 export default function HomePage() {
 	return (
 		<main>
+			<DevelopmentTable />
 			<FourColumnTable />
 		</main>
 	);
