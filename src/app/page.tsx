bc3559ca3b9918eae50b60a5282a81b6b586129d
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-01 19:31:29 +0700

    Add check table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CheckTable } from "./check-table";
 import { DevelopmentTable } from "./development-table";
 import { FourColumnTable } from "./four-column-table";
 
@@ -5,6 +6,7 @@ export default function HomePage() {
 	return (
 		<main>
 			<DevelopmentTable />
+			<CheckTable />
 			<FourColumnTable />
 		</main>
 	);
