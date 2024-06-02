06963e69135c29845f47c533478a109ebdc3437f
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-02 22:05:23 +0700

    Add dialog styles

--- src/app/data-tables/development-table.tsx
+++ src/app/data-tables/development-table.tsx
@@ -18,6 +18,9 @@ import {
 	Calendar,
 	CalendarCell,
 	CalendarGrid,
+	CalendarGridBody,
+	CalendarGridHeader,
+	CalendarHeaderCell,
 	Checkbox,
 	CheckboxGroup,
 	DateInput,
@@ -44,7 +47,7 @@ import {
 	IoLogoApple,
 	IoLogoWindows,
 } from "react-icons/io";
-import { MdCheck } from "react-icons/md";
+import { MdCheck, MdDelete } from "react-icons/md";
 import { InferType, array, mixed, object, string } from "yup";
 
 type Technology = {
@@ -96,14 +99,22 @@ const defaultData: Product[] = [
 const columnHelper = createColumnHelper<Product>();
 
 const schema = object({
-	name: string().required().default(""),
-	technologies: array().of(string().required()).min(1).default([]),
+	name: string().label("Name").required().default(""),
+	technologies: array()
+		.of(string().required())
+		.label("Tech")
+		.min(1)
+		.default([]),
 	date: mixed<DateValue>()
 		.defined()
 		.nullable()
 		.default(null)
-		.test("non-nullable", "Date is required", (value) => value !== null),
-	progress: string().required().default(""),
+		.test(
+			"non-nullable",
+			"Date is a required field",
+			(value) => value !== null,
+		),
+	progress: string().label("Progress").required().default(""),
 });
 
 type Schema = InferType<typeof schema>;
@@ -162,7 +173,9 @@ export function DevelopmentTable() {
 				header: () => "Actions",
 				cell: (props) => (
 					<DialogTrigger>
-						<Button>Delete</Button>
+						<Button>
+							<MdDelete className="text-red-500" size={20} />
+						</Button>
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
@@ -250,7 +263,20 @@ export function DevelopmentTable() {
 						Development Table
 					</h2>
 
-					<DialogTrigger>
+					<DialogTrigger
+						onOpenChange={(isOpen) => {
+							if (!isOpen) {
+								setTimeout(() => {
+									form.reset({
+										name: "",
+										technologies: [],
+										date: null,
+										progress: "",
+									});
+								}, 500);
+							}
+						}}
+					>
 						<Button
 							type="button"
 							className="h-10 flex-shrink-0 rounded-[70px] bg-[#11047A] px-6 text-sm font-medium text-white shadow-[rgba(112,144,176,0.08)_45px_76px_113px_7px]"
@@ -258,139 +284,205 @@ export function DevelopmentTable() {
 							Add Product
 						</Button>
 
-						<Modal>
-							<Dialog>
-								{({ close }) => (
-									<form
-										onSubmit={form.handleSubmit((data) => {
-											const relatedTechnologies = technologies.filter(
-												(technology) =>
-													data.technologies.includes(technology.id),
-											);
+						<ModalOverlay
+							className={({ isEntering, isExiting }) =>
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+							}
+						>
+							<Modal
+								className={({ isEntering, isExiting }) =>
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+								}
+							>
+								<Dialog role="alertdialog" className="relative outline-none">
+									{({ close }) => (
+										<form
+											onSubmit={form.handleSubmit((data) => {
+												const relatedTechnologies = technologies.filter(
+													(technology) =>
+														data.technologies.includes(technology.id),
+												);
 
-											setData((prev) => [
-												{
-													name: data.name,
-													technologies: relatedTechnologies,
-													progress: Number(data.progress),
-													date: dayjs(
-														data.date?.toString(),
-														"YYYY-MM-DD",
-													).toDate(),
-												},
-												...prev,
-											]);
+												setData((prev) => [
+													{
+														name: data.name,
+														technologies: relatedTechnologies,
+														progress: Number(data.progress),
+														date: dayjs(
+															data.date?.toString(),
+															"YYYY-MM-DD",
+														).toDate(),
+													},
+													...prev,
+												]);
 
-											close();
-										})}
-									>
-										<Heading slot="title">Add Product</Heading>
-										<Controller
-											control={form.control}
-											name="name"
-											render={(renderProps) => (
-												<TextField
-													{...renderProps.field}
-													isInvalid={renderProps.fieldState.invalid}
-													autoFocus
+												close();
+											})}
+										>
+											<Heading
+												slot="title"
+												className="my-0 text-2xl font-semibold leading-6 text-slate-700"
+											>
+												Add Product
+											</Heading>
+
+											<div className="mt-4 flex flex-col gap-y-4">
+												<Controller
+													control={form.control}
+													name="name"
+													render={(renderProps) => (
+														<TextField
+															{...renderProps.field}
+															isInvalid={renderProps.fieldState.invalid}
+															autoFocus
+															className="flex flex-col gap-1"
+														>
+															<Label className="w-fit cursor-default text-sm font-medium text-gray-500">
+																Name
+															</Label>
+															<Input className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" />
+															<FieldError className="text-sm text-red-600">
+																{renderProps.fieldState.error?.message}
+															</FieldError>
+														</TextField>
+													)}
+												/>
+
+												<Controller
+													control={form.control}
+													name="technologies"
+													render={(renderProps) => (
+														<CheckboxGroup
+															{...renderProps.field}
+															isInvalid={renderProps.fieldState.invalid}
+															className="flex flex-col gap-1"
+														>
+															<Label className="w-fit cursor-default text-sm font-medium text-gray-500">
+																Tech
+															</Label>
+
+															{technologies.map((technology) => (
+																<Checkbox
+																	key={technology.id}
+																	value={technology.id}
+																>
+																	{({ isSelected }) => (
+																		<div className="flex items-center">
+																			<div className="flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition">
+																				{isSelected ? <MdCheck /> : null}
+																			</div>
+																			{technology.name}
+																		</div>
+																	)}
+																</Checkbox>
+															))}
+
+															<FieldError className="text-sm text-red-600">
+																{renderProps.fieldState.error?.message}
+															</FieldError>
+														</CheckboxGroup>
+													)}
+												/>
+
+												<Controller
+													control={form.control}
+													name="date"
+													render={(renderProps) => (
+														<DatePicker
+															{...renderProps.field}
+															isInvalid={renderProps.fieldState.invalid}
+															className="group flex flex-col gap-1"
+														>
+															<Label className="w-fit cursor-default text-sm font-medium text-gray-500">
+																Date
+															</Label>
+															<Group className="group flex h-9 w-auto min-w-[208px] items-center overflow-hidden rounded-lg border-2 bg-white forced-colors:bg-[Field]">
+																<DateInput className="block min-w-[150px] flex-1 px-2 py-1.5 text-sm">
+																	{(segment) => (
+																		<DateSegment
+																			className="type-literal:px-0 inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]"
+																			segment={segment}
+																		/>
+																	)}
+																</DateInput>
+																<Button>▼</Button>
+															</Group>
+															<FieldError className="text-sm text-red-600">
+																{renderProps.fieldState.error?.message}
+															</FieldError>
+															<Popover>
+																<Dialog className="relative max-h-[inherit] overflow-auto p-6 outline outline-0 [[data-placement]>&]:p-4">
+																	<Calendar>
+																		<header className="flex w-full items-center gap-1 px-1 pb-4">
+																			<Button slot="previous">◀</Button>
+																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900 dark:text-zinc-200" />
+																			<Button slot="next">▶</Button>
+																		</header>
+																		<CalendarGrid>
+																			<CalendarGridHeader>
+																				{(day) => (
+																					<CalendarHeaderCell className="text-xs font-semibold text-gray-500">
+																						{day}
+																					</CalendarHeaderCell>
+																				)}
+																			</CalendarGridHeader>
+																			<CalendarGridBody>
+																				{(date) => (
+																					<CalendarCell
+																						date={date}
+																						className="flex h-9 w-9 cursor-default items-center justify-center rounded-full text-sm forced-color-adjust-none"
+																					/>
+																				)}
+																			</CalendarGridBody>
+																		</CalendarGrid>
+																	</Calendar>
+																</Dialog>
+															</Popover>
+														</DatePicker>
+													)}
+												/>
+
+												<Controller
+													control={form.control}
+													name="progress"
+													render={(renderProps) => (
+														<TextField
+															{...renderProps.field}
+															isInvalid={renderProps.fieldState.invalid}
+															className="flex flex-col gap-1"
+														>
+															<Label className="w-fit cursor-default text-sm font-medium text-gray-500">
+																Progress
+															</Label>
+															<Input className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" />
+															<FieldError className="text-sm text-red-600">
+																{renderProps.fieldState.error?.message}
+															</FieldError>
+														</TextField>
+													)}
+												/>
+											</div>
+
+											<div className="mt-6 flex justify-end gap-2">
+												<Button
+													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													onPress={close}
 												>
-													<Label>Name</Label>
-													<Input />
-													<FieldError>
-														{renderProps.fieldState.error?.message}
-													</FieldError>
-												</TextField>
-											)}
-										/>
+													Cancel
+												</Button>
 
-										<Controller
-											control={form.control}
-											name="technologies"
-											render={(renderProps) => (
-												<CheckboxGroup
-													{...renderProps.field}
-													isInvalid={renderProps.fieldState.invalid}
+												<Button
+													type="submit"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-[#11047A] px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2"
 												>
-													<Label>Tech</Label>
-
-													{technologies.map((technology) => (
-														<Checkbox key={technology.id} value={technology.id}>
-															{({ isSelected }) => (
-																<div className="flex items-center">
-																	<div className="flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition">
-																		{isSelected ? <MdCheck /> : null}
-																	</div>
-																	{technology.name}
-																</div>
-															)}
-														</Checkbox>
-													))}
-
-													<FieldError>
-														{renderProps.fieldState.error?.message}
-													</FieldError>
-												</CheckboxGroup>
-											)}
-										/>
-
-										<Controller
-											control={form.control}
-											name="date"
-											render={(renderProps) => (
-												<DatePicker
-													{...renderProps.field}
-													isInvalid={renderProps.fieldState.invalid}
-												>
-													<Label>Date</Label>
-													<Group>
-														<DateInput>
-															{(segment) => <DateSegment segment={segment} />}
-														</DateInput>
-														<Button>▼</Button>
-													</Group>
-													<FieldError>
-														{renderProps.fieldState.error?.message}
-													</FieldError>
-													<Popover>
-														<Dialog>
-															<Calendar>
-																<header>
-																	<Button slot="previous">◀</Button>
-																	<Heading />
-																	<Button slot="next">▶</Button>
-																</header>
-																<CalendarGrid>
-																	{(date) => <CalendarCell date={date} />}
-																</CalendarGrid>
-															</Calendar>
-														</Dialog>
-													</Popover>
-												</DatePicker>
-											)}
-										/>
-
-										<Controller
-											control={form.control}
-											name="progress"
-											render={(renderProps) => (
-												<TextField
-													{...renderProps.field}
-													isInvalid={renderProps.fieldState.invalid}
-												>
-													<Label>Progress</Label>
-													<Input />
-													<FieldError>
-														{renderProps.fieldState.error?.message}
-													</FieldError>
-												</TextField>
-											)}
-										/>
-
-										<Button type="submit">Submit</Button>
-									</form>
-								)}
-							</Dialog>
-						</Modal>
+													Submit
+												</Button>
+											</div>
+										</form>
+									)}
+								</Dialog>
+							</Modal>
+						</ModalOverlay>
 					</DialogTrigger>
 				</div>
 
