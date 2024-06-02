f5e9af030fedcbec2a0f81929c5f6868f486ef48
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-03 05:31:10 +0700

    Fix development table tech & date fields

--- src/app/data-tables/development-table.tsx
+++ src/app/data-tables/development-table.tsx
@@ -41,6 +41,7 @@ import {
 } from "react-aria-components";
 import { Controller, useForm } from "react-hook-form";
 import type { IconType } from "react-icons";
+import { BiSolidDownArrow } from "react-icons/bi";
 import {
 	IoIosSearch,
 	IoLogoAndroid,
@@ -179,12 +180,12 @@ export function DevelopmentTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog role="alertdialog" className="relative outline-none">
@@ -202,7 +203,7 @@ export function DevelopmentTable() {
 											</p>
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
@@ -218,7 +219,7 @@ export function DevelopmentTable() {
 
 														close();
 													}}
-													className="pressed:bg-red-600 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2 pressed:bg-red-600"
 												>
 													Delete
 												</Button>
@@ -286,12 +287,12 @@ export function DevelopmentTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog className="relative outline-none">
@@ -367,7 +368,7 @@ export function DevelopmentTable() {
 																	value={technology.id}
 																>
 																	{({ isSelected }) => (
-																		<div className="flex items-center">
+																		<div className="flex items-center gap-x-2">
 																			<div className="flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition">
 																				{isSelected ? <MdCheck /> : null}
 																			</div>
@@ -400,22 +401,24 @@ export function DevelopmentTable() {
 																<DateInput className="block min-w-[150px] flex-1 px-2 py-1.5 text-sm">
 																	{(segment) => (
 																		<DateSegment
-																			className="type-literal:px-0 inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]"
+																			className="inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none type-literal:px-0 forced-colors:text-[ButtonText]"
 																			segment={segment}
 																		/>
 																	)}
 																</DateInput>
-																<Button>▼</Button>
+																<Button className="px-2">
+																	<BiSolidDownArrow />
+																</Button>
 															</Group>
 															<FieldError className="text-sm text-red-600">
 																{renderProps.fieldState.error?.message}
 															</FieldError>
-															<Popover>
+															<Popover className="rounded-xl border border-black/10 bg-white bg-clip-padding text-slate-700 shadow-2xl forced-colors:bg-[Canvas]">
 																<Dialog className="relative max-h-[inherit] overflow-auto p-6 outline outline-0 [[data-placement]>&]:p-4">
 																	<Calendar>
 																		<header className="flex w-full items-center gap-1 px-1 pb-4">
 																			<Button slot="previous">◀</Button>
-																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900 dark:text-zinc-200" />
+																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900" />
 																			<Button slot="next">▶</Button>
 																		</header>
 																		<CalendarGrid>
@@ -465,7 +468,7 @@ export function DevelopmentTable() {
 
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
