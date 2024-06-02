8d45cac949a9eefab10e058ac379487c1fb0b741
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-02 12:10:25 +0700

    Add table styles

--- src/app/development-table.tsx
+++ src/app/development-table.tsx
@@ -35,7 +35,12 @@ import {
 } from "react-aria-components";
 import { Controller, useForm } from "react-hook-form";
 import type { IconType } from "react-icons";
-import { IoLogoAndroid, IoLogoApple, IoLogoWindows } from "react-icons/io";
+import {
+	IoIosSearch,
+	IoLogoAndroid,
+	IoLogoApple,
+	IoLogoWindows,
+} from "react-icons/io";
 import { MdCheck } from "react-icons/md";
 import { InferType, array, mixed, object, string } from "yup";
 
@@ -114,9 +119,9 @@ export function DevelopmentTable() {
 			columnHelper.accessor("technologies", {
 				header: () => "Tech",
 				cell: (info) => (
-					<div className="flex items-center">
+					<div className="flex items-center gap-x-4">
 						{info.getValue().map((technology) => (
-							<technology.icon key={technology.id} />
+							<technology.icon key={technology.id} size={22} color="#8F9BBA" />
 						))}
 					</div>
 				),
@@ -132,11 +137,14 @@ export function DevelopmentTable() {
 			columnHelper.accessor("progress", {
 				header: () => "Progress",
 				cell: (info) => (
-					<div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-						<div
-							className="h-2.5 rounded-full bg-blue-600"
-							style={{ width: `${info.getValue()}%` }}
-						/>
+					<div className="flex items-center gap-x-2.5">
+						${info.getValue()}%
+						<div className="h-2 w-full rounded-full bg-[#EFF4FB]">
+							<div
+								className="h-2 rounded-full bg-[#422AFB]"
+								style={{ width: `${info.getValue()}%` }}
+							/>
+						</div>
 					</div>
 				),
 				enableColumnFilter: false,
@@ -144,6 +152,7 @@ export function DevelopmentTable() {
 			}),
 			columnHelper.display({
 				id: "actions",
+				header: () => "Actions",
 				cell: (props) => (
 					<DialogTrigger>
 						<Button>Delete</Button>
@@ -203,183 +212,209 @@ export function DevelopmentTable() {
 	});
 
 	return (
-		<div>
-			<div className="flex items-center">
-				<h2>Development Table</h2>
+		<div className="flex flex-col gap-y-5 rounded-[20px] bg-white py-5">
+			<div className="flex flex-col gap-y-4 px-[1.5625rem]">
+				<div className="flex items-center justify-between">
+					<h2 className="text-[1.375rem] font-bold leading-[100%] text-[#1B2559]">
+						Development Table
+					</h2>
 
-				<DialogTrigger>
-					<Button>Add Product</Button>
+					<DialogTrigger>
+						<Button
+							type="button"
+							className="h-10 rounded-[70px] bg-[#11047A] px-6 text-sm font-medium text-white shadow-[rgba(112,144,176,0.08)_45px_76px_113px_7px]"
+						>
+							Add Product
+						</Button>
 
-					<Modal>
-						<Dialog>
-							{({ close }) => (
-								<form
-									onSubmit={form.handleSubmit((data) => {
-										const relatedTechnologies = technologies.filter(
-											(technology) => data.technologies.includes(technology.id),
-										);
+						<Modal>
+							<Dialog>
+								{({ close }) => (
+									<form
+										onSubmit={form.handleSubmit((data) => {
+											const relatedTechnologies = technologies.filter(
+												(technology) =>
+													data.technologies.includes(technology.id),
+											);
 
-										setData((prev) => [
-											{
-												name: data.name,
-												technologies: relatedTechnologies,
-												progress: Number(data.progress),
-												date: dayjs(
-													data.date?.toString(),
-													"YYYY-MM-DD",
-												).toDate(),
-											},
-											...prev,
-										]);
+											setData((prev) => [
+												{
+													name: data.name,
+													technologies: relatedTechnologies,
+													progress: Number(data.progress),
+													date: dayjs(
+														data.date?.toString(),
+														"YYYY-MM-DD",
+													).toDate(),
+												},
+												...prev,
+											]);
 
-										close();
-									})}
-								>
-									<Heading slot="title">Add Product</Heading>
-									<Controller
-										control={form.control}
-										name="name"
-										render={(renderProps) => (
-											<TextField
-												{...renderProps.field}
-												isInvalid={renderProps.fieldState.invalid}
-												autoFocus
-											>
-												<Label>Name</Label>
-												<Input />
-												<FieldError>
-													{renderProps.fieldState.error?.message}
-												</FieldError>
-											</TextField>
-										)}
-									/>
+											close();
+										})}
+									>
+										<Heading slot="title">Add Product</Heading>
+										<Controller
+											control={form.control}
+											name="name"
+											render={(renderProps) => (
+												<TextField
+													{...renderProps.field}
+													isInvalid={renderProps.fieldState.invalid}
+													autoFocus
+												>
+													<Label>Name</Label>
+													<Input />
+													<FieldError>
+														{renderProps.fieldState.error?.message}
+													</FieldError>
+												</TextField>
+											)}
+										/>
 
-									<Controller
-										control={form.control}
-										name="technologies"
-										render={(renderProps) => (
-											<CheckboxGroup
-												{...renderProps.field}
-												isInvalid={renderProps.fieldState.invalid}
-											>
-												<Label>Tech</Label>
+										<Controller
+											control={form.control}
+											name="technologies"
+											render={(renderProps) => (
+												<CheckboxGroup
+													{...renderProps.field}
+													isInvalid={renderProps.fieldState.invalid}
+												>
+													<Label>Tech</Label>
 
-												{technologies.map((technology) => (
-													<Checkbox key={technology.id} value={technology.id}>
-														{({ isSelected }) => (
-															<div className="flex items-center">
-																<div className="flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition">
-																	{isSelected ? <MdCheck /> : null}
+													{technologies.map((technology) => (
+														<Checkbox key={technology.id} value={technology.id}>
+															{({ isSelected }) => (
+																<div className="flex items-center">
+																	<div className="flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition">
+																		{isSelected ? <MdCheck /> : null}
+																	</div>
+																	{technology.name}
 																</div>
-																{technology.name}
-															</div>
-														)}
-													</Checkbox>
-												))}
+															)}
+														</Checkbox>
+													))}
 
-												<FieldError>
-													{renderProps.fieldState.error?.message}
-												</FieldError>
-											</CheckboxGroup>
-										)}
-									/>
+													<FieldError>
+														{renderProps.fieldState.error?.message}
+													</FieldError>
+												</CheckboxGroup>
+											)}
+										/>
 
-									<Controller
-										control={form.control}
-										name="date"
-										render={(renderProps) => (
-											<DatePicker
-												{...renderProps.field}
-												isInvalid={renderProps.fieldState.invalid}
-											>
-												<Label>Date</Label>
-												<Group>
-													<DateInput>
-														{(segment) => <DateSegment segment={segment} />}
-													</DateInput>
-													<Button>▼</Button>
-												</Group>
-												<FieldError>
-													{renderProps.fieldState.error?.message}
-												</FieldError>
-												<Popover>
-													<Dialog>
-														<Calendar>
-															<header>
-																<Button slot="previous">◀</Button>
-																<Heading />
-																<Button slot="next">▶</Button>
-															</header>
-															<CalendarGrid>
-																{(date) => <CalendarCell date={date} />}
-															</CalendarGrid>
-														</Calendar>
-													</Dialog>
-												</Popover>
-											</DatePicker>
-										)}
-									/>
+										<Controller
+											control={form.control}
+											name="date"
+											render={(renderProps) => (
+												<DatePicker
+													{...renderProps.field}
+													isInvalid={renderProps.fieldState.invalid}
+												>
+													<Label>Date</Label>
+													<Group>
+														<DateInput>
+															{(segment) => <DateSegment segment={segment} />}
+														</DateInput>
+														<Button>▼</Button>
+													</Group>
+													<FieldError>
+														{renderProps.fieldState.error?.message}
+													</FieldError>
+													<Popover>
+														<Dialog>
+															<Calendar>
+																<header>
+																	<Button slot="previous">◀</Button>
+																	<Heading />
+																	<Button slot="next">▶</Button>
+																</header>
+																<CalendarGrid>
+																	{(date) => <CalendarCell date={date} />}
+																</CalendarGrid>
+															</Calendar>
+														</Dialog>
+													</Popover>
+												</DatePicker>
+											)}
+										/>
 
-									<Controller
-										control={form.control}
-										name="progress"
-										render={(renderProps) => (
-											<TextField
-												{...renderProps.field}
-												isInvalid={renderProps.fieldState.invalid}
-											>
-												<Label>Progress</Label>
-												<Input />
-												<FieldError>
-													{renderProps.fieldState.error?.message}
-												</FieldError>
-											</TextField>
-										)}
-									/>
+										<Controller
+											control={form.control}
+											name="progress"
+											render={(renderProps) => (
+												<TextField
+													{...renderProps.field}
+													isInvalid={renderProps.fieldState.invalid}
+												>
+													<Label>Progress</Label>
+													<Input />
+													<FieldError>
+														{renderProps.fieldState.error?.message}
+													</FieldError>
+												</TextField>
+											)}
+										/>
 
-									<Button type="submit">Submit</Button>
-								</form>
-							)}
-						</Dialog>
-					</Modal>
-				</DialogTrigger>
+										<Button type="submit">Submit</Button>
+									</form>
+								)}
+							</Dialog>
+						</Modal>
+					</DialogTrigger>
+				</div>
+
+				<div className="flex items-center overflow-hidden rounded-2xl bg-[#F4F7FE]">
+					<div className="flex h-10 w-10 flex-shrink-0 items-center justify-center">
+						<IoIosSearch size={20} color="#2D3748" />
+					</div>
+
+					<input
+						type="text"
+						value={globalFilter}
+						onChange={(e) => setGlobalFilter(e.target.value)}
+						className="h-10 flex-1 bg-inherit pr-5 focus-visible:outline-none"
+						placeholder="Search..."
+					/>
+				</div>
 			</div>
 
-			<input
-				type="text"
-				value={globalFilter}
-				onChange={(e) => setGlobalFilter(e.target.value)}
-			/>
-
-			<table>
-				<thead>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map((header) => (
-								<th key={header.id}>
-									{header.isPlaceholder
-										? null
-										: flexRender(
-												header.column.columnDef.header,
-												header.getContext(),
-											)}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody>
-					{table.getRowModel().rows.map((row) => (
-						<tr key={row.id}>
-							{row.getVisibleCells().map((cell) => (
-								<td key={cell.id}>
-									{flexRender(cell.column.columnDef.cell, cell.getContext())}
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<div className="relative overflow-x-auto">
+				<table>
+					<thead>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<th
+										className="border-b border-slate-200 py-3 pl-6 pr-[0.625rem] text-left text-[0.625rem] font-bold uppercase tracking-wider text-[#A0AEC0] md:text-xs"
+										key={header.id}
+									>
+										{header.isPlaceholder
+											? null
+											: flexRender(
+													header.column.columnDef.header,
+													header.getContext(),
+												)}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody>
+						{table.getRowModel().rows.map((row) => (
+							<tr key={row.id}>
+								{row.getVisibleCells().map((cell) => (
+									<td
+										key={cell.id}
+										className="min-w-[9.375rem] py-3 pl-6 pr-[0.625rem] text-sm font-bold text-[#1B2559]"
+									>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,14 @@ import { FourColumnTable } from "./four-column-table";
 
 export default function HomePage() {
 	return (
-		<main className="grid grid-cols-2">
-			<DevelopmentTable />
-			<CheckTable />
-			<FourColumnTable />
-			<ComplexTable />
+		<main className="px-5 pb-5 pt-[3.125rem]">
+			<h1 className="text-[2.125rem] font-bold text-[#1B254B]">Data Tables</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2">
+				<DevelopmentTable />
+				<CheckTable />
+				<FourColumnTable />
+				<ComplexTable />
+			</div>
 		</main>
 	);
 }
