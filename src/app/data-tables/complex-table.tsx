9e1f63a8d86a346688bccee33c4c1d172bc92f9f
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-03 05:55:08 +0700

    Fix complex table date & select fields

--- src/app/data-tables/complex-table.tsx
+++ src/app/data-tables/complex-table.tsx
@@ -44,6 +44,7 @@ import {
 } from "react-aria-components";
 import { Controller, useForm } from "react-hook-form";
 import { IconType } from "react-icons";
+import { BiSolidDownArrow } from "react-icons/bi";
 import {
 	IoIosCheckmarkCircle,
 	IoIosSearch,
@@ -194,12 +195,12 @@ export function ComplexTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog role="alertdialog" className="relative outline-none">
@@ -217,7 +218,7 @@ export function ComplexTable() {
 											</p>
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
@@ -233,7 +234,7 @@ export function ComplexTable() {
 
 														close();
 													}}
-													className="pressed:bg-red-600 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2 pressed:bg-red-600"
 												>
 													Delete
 												</Button>
@@ -301,12 +302,12 @@ export function ComplexTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog className="relative outline-none">
@@ -385,17 +386,24 @@ export function ComplexTable() {
 															<Label className="w-fit cursor-default text-sm font-medium text-gray-500">
 																Status
 															</Label>
-															<Button>
-																<SelectValue />
-																<span aria-hidden="true">▼</span>
+															<Button className="flex w-full min-w-[150px] cursor-default items-center gap-4 rounded-lg border border-black/10 bg-gray-50 py-2 pl-3 pr-2 text-start shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] transition">
+																<SelectValue className="flex-1 text-sm placeholder-shown:italic" />
+																<BiSolidDownArrow
+																	aria-hidden
+																	className="h-4 w-4 group-disabled:text-gray-200 forced-colors:text-[ButtonText] forced-colors:group-disabled:text-[GrayText]"
+																/>
 															</Button>
-															<FieldError>
+															<FieldError className="text-sm text-red-600">
 																{fieldState.error?.message}
 															</FieldError>
-															<Popover>
-																<ListBox>
+															<Popover className="min-w-[--trigger-width] rounded-xl border border-black/10 bg-white bg-clip-padding text-slate-700 shadow-2xl forced-colors:bg-[Canvas]">
+																<ListBox className="max-h-[inherit] overflow-auto p-1 outline-none [clip-path:inset(0_0_0_0_round_.75rem)]">
 																	{status.map((s) => (
-																		<ListBoxItem id={s.id} key={s.id}>
+																		<ListBoxItem
+																			id={s.id}
+																			key={s.id}
+																			className="group flex cursor-default select-none items-center gap-4 rounded-lg py-2 pl-3 pr-1 text-sm outline outline-0 forced-color-adjust-none"
+																		>
 																			{s.name}
 																		</ListBoxItem>
 																	))}
@@ -421,22 +429,24 @@ export function ComplexTable() {
 																<DateInput className="block min-w-[150px] flex-1 px-2 py-1.5 text-sm">
 																	{(segment) => (
 																		<DateSegment
-																			className="type-literal:px-0 inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]"
+																			className="inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none type-literal:px-0 forced-colors:text-[ButtonText]"
 																			segment={segment}
 																		/>
 																	)}
 																</DateInput>
-																<Button>▼</Button>
+																<Button className="px-2">
+																	<BiSolidDownArrow />
+																</Button>
 															</Group>
 															<FieldError className="text-sm text-red-600">
 																{renderProps.fieldState.error?.message}
 															</FieldError>
-															<Popover>
+															<Popover className="rounded-xl border border-black/10 bg-white bg-clip-padding text-slate-700 shadow-2xl forced-colors:bg-[Canvas]">
 																<Dialog className="relative max-h-[inherit] overflow-auto p-6 outline outline-0 [[data-placement]>&]:p-4">
 																	<Calendar>
 																		<header className="flex w-full items-center gap-1 px-1 pb-4">
 																			<Button slot="previous">◀</Button>
-																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900 dark:text-zinc-200" />
+																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900" />
 																			<Button slot="next">▶</Button>
 																		</header>
 																		<CalendarGrid>
@@ -486,7 +496,7 @@ export function ComplexTable() {
 
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
