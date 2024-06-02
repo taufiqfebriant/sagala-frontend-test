0dcd9168d870cc9461c7618237c985ac45c58ba3
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-06-03 05:37:36 +0700

    Fix 4-column table date field

--- src/app/data-tables/four-column-table.tsx
+++ src/app/data-tables/four-column-table.tsx
@@ -38,6 +38,7 @@ import {
 	TextField,
 } from "react-aria-components";
 import { Controller, useForm } from "react-hook-form";
+import { BiSolidDownArrow } from "react-icons/bi";
 import { IoIosSearch } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
 import { InferType, mixed, object, string } from "yup";
@@ -127,12 +128,12 @@ export function FourColumnTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog role="alertdialog" className="relative outline-none">
@@ -150,7 +151,7 @@ export function FourColumnTable() {
 											</p>
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
@@ -166,7 +167,7 @@ export function FourColumnTable() {
 
 														close();
 													}}
-													className="pressed:bg-red-600 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-red-500 px-5 py-2 font-[inherit] text-base font-semibold text-white outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-red-600 focus-visible:ring-2 pressed:bg-red-600"
 												>
 													Delete
 												</Button>
@@ -234,12 +235,12 @@ export function FourColumnTable() {
 
 						<ModalOverlay
 							className={({ isEntering, isExiting }) =>
-								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "animate-in fade-in duration-300 ease-out" : ""} ${isExiting ? "animate-out fade-out duration-200 ease-in" : ""} `
+								`fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur ${isEntering ? "duration-300 ease-out animate-in fade-in" : ""} ${isExiting ? "duration-200 ease-in animate-out fade-out" : ""} `
 							}
 						>
 							<Modal
 								className={({ isEntering, isExiting }) =>
-									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "animate-in zoom-in-95 duration-300 ease-out" : ""} ${isExiting ? "animate-out zoom-out-95 duration-200 ease-in" : ""} `
+									`w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl ${isEntering ? "duration-300 ease-out animate-in zoom-in-95" : ""} ${isExiting ? "duration-200 ease-in animate-out zoom-out-95" : ""} `
 								}
 							>
 								<Dialog className="relative outline-none">
@@ -347,22 +348,24 @@ export function FourColumnTable() {
 																<DateInput className="block min-w-[150px] flex-1 px-2 py-1.5 text-sm">
 																	{(segment) => (
 																		<DateSegment
-																			className="type-literal:px-0 inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]"
+																			className="inline rounded p-0.5 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none type-literal:px-0 forced-colors:text-[ButtonText]"
 																			segment={segment}
 																		/>
 																	)}
 																</DateInput>
-																<Button>▼</Button>
+																<Button className="px-2">
+																	<BiSolidDownArrow />
+																</Button>
 															</Group>
 															<FieldError className="text-sm text-red-600">
 																{renderProps.fieldState.error?.message}
 															</FieldError>
-															<Popover>
+															<Popover className="rounded-xl border border-black/10 bg-white bg-clip-padding text-slate-700 shadow-2xl forced-colors:bg-[Canvas]">
 																<Dialog className="relative max-h-[inherit] overflow-auto p-6 outline outline-0 [[data-placement]>&]:p-4">
 																	<Calendar>
 																		<header className="flex w-full items-center gap-1 px-1 pb-4">
 																			<Button slot="previous">◀</Button>
-																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900 dark:text-zinc-200" />
+																			<Heading className="mx-2 flex-1 text-center text-xl font-semibold text-zinc-900" />
 																			<Button slot="next">▶</Button>
 																		</header>
 																		<CalendarGrid>
@@ -392,7 +395,7 @@ export function FourColumnTable() {
 
 											<div className="mt-6 flex justify-end gap-2">
 												<Button
-													className="pressed:bg-slate-300 inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2"
+													className="inline-flex cursor-default justify-center rounded-md border border-solid border-transparent bg-slate-200 px-5 py-2 font-[inherit] text-base font-semibold text-slate-800 outline-none ring-blue-500 ring-offset-2 transition-colors hover:border-slate-300 focus-visible:ring-2 pressed:bg-slate-300"
 													onPress={close}
 												>
 													Cancel
